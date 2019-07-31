37a11b456c146b3deebbfda144b364dcc787ff30
Chang__ <chang_avalon@163.com>
2019-08-01 00:04:48 +0800

    1.设计系统模块的数据表结构(系统权限模块); 2.创建PO类与repository接口;

---
+++ springboot-template-starter-domain/src/main/java/com/example/boot/springboottemplatedomain/page/payload/CreatePagePLO.java
@@ -0,0 +1,10 @@
+package com.example.boot.springboottemplatedomain.page.payload;
+
+/**
+ * write this class description...
+ *
+ * @author Chang
+ * @date 2019/8/1 0:04
+ */
+public class CreatePagePLO {
+}
