e2673d7cf6ef4230339741119af145a99f792cce
Chang__ <chang_avalon@163.com>
2019-08-12 23:17:34 +0800

    1.开发[系统权限]模块中...

--- springboot-template-starter-domain/src/main/java/com/example/boot/springboottemplatedomain/permission/response/ModifyPermissionRO.java
+++ springboot-template-starter-domain/src/main/java/com/example/boot/springboottemplatedomain/permission/response/ModifyPermissionRO.java
@@ -1,9 +1,13 @@
 package com.example.boot.springboottemplatedomain.permission.response;
 
+import cn.hutool.core.bean.BeanUtil;
+import com.example.boot.springboottemplatedomain.permission.persistent.SystemPermission;
+import com.example.boot.springboottemplatedomain.permission.persistent.SystemPermissionUrl;
 import lombok.Data;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * write this class description...
@@ -18,6 +22,22 @@ public class ModifyPermissionRO {
     private String name;
     private String code;
     private List<PermissionUrl> permissionUrls = new ArrayList<>();
+
+    public static ModifyPermissionRO createPermissionRO(SystemPermission permission, List<SystemPermissionUrl> permissionUrls) {
+        ModifyPermissionRO permissionRO = new ModifyPermissionRO();
+        BeanUtil.copyProperties(permission, permissionRO);
+
+        List<PermissionUrl> urlROS = permissionUrls.stream().map(permissionUrl -> {
+            PermissionUrl urlRO = new PermissionUrl();
+            BeanUtil.copyProperties(permissionUrl, urlRO);
+
+            return urlRO;
+        }).collect(Collectors.toList());
+
+        permissionRO.setPermissionUrls(urlROS);
+
+        return permissionRO;
+    }
 
     @Data
     public static class PermissionUrl {
