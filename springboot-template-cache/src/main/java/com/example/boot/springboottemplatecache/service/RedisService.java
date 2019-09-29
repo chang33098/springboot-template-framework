e3234d710c41492a0c80c35fdffd3755460fd947
Chang__ <chang_avalon@163.com>
2019-09-29 23:44:38 +0800

    合并代码

--- springboot-template-cache/src/main/java/com/example/boot/springboottemplatecache/service/RedisService.java
+++ springboot-template-cache/src/main/java/com/example/boot/springboottemplatecache/service/RedisService.java
@@ -1,7 +1,11 @@
 package com.example.boot.springboottemplatecache.service;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by EDZ on 2019/9/25.
  */
 public interface RedisService {
+
+    void putAndSetExpired(String key, String value, Long expireTime, TimeUnit unit);
 }
--- springboot-template-cache/src/main/java/com/example/boot/springboottemplatecache/service/impl/RedisServiceImpl.java
+++ springboot-template-cache/src/main/java/com/example/boot/springboottemplatecache/service/impl/RedisServiceImpl.java
@@ -1,11 +1,27 @@
 package com.example.boot.springboottemplatecache.service.impl;
 
 import com.example.boot.springboottemplatecache.service.RedisService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by EDZ on 2019/9/25.
  */
 @Service
 public class RedisServiceImpl implements RedisService {
+
+    private final StringRedisTemplate redisTemplate;
+
+    @Autowired
+    public RedisServiceImpl(StringRedisTemplate redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    @Override
+    public void putAndSetExpired(String key, String value, Long expireTime, TimeUnit unit) {
+        redisTemplate.opsForValue().set(key, value, expireTime, unit);
+    }
 }
