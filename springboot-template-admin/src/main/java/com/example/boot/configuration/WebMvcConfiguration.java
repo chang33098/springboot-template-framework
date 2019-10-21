c8a15f08e25e531c987a725bc9f453e5d48c5151
Chang__ <chang_avalon@163.com>
2019-10-22 01:15:56 +0800

    thymeleaf自定了处理LayUI标签的方言和处理器;

--- springboot-template-admin/src/main/java/com/example/boot/configuration/WebMvcConfiguration.java
+++ springboot-template-admin/src/main/java/com/example/boot/configuration/WebMvcConfiguration.java
@@ -4,12 +4,17 @@ import cn.hutool.core.lang.Dict;
 import com.example.boot.properties.CustomResourceConfiguration;
 import com.example.boot.properties.CustomViewConfiguration;
 import com.example.boot.properties.CustomUploadConfiguration;
+import com.example.boot.thymeleaf.LayUITagDialect;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.server.ConfigurableWebServerFactory;
 import org.springframework.boot.web.server.ErrorPage;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
@@ -17,6 +22,9 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 import javax.annotation.Resource;
@@ -31,43 +39,34 @@ import java.util.Map;
 @Slf4j
 @Configuration
 @EnableConfigurationProperties({
+        ThymeleafProperties.class,
         CustomViewConfiguration.class,
         CustomResourceConfiguration.class,
         CustomUploadConfiguration.class
 })
-public class WebMvcConfiguration implements WebMvcConfigurer {
+public class WebMvcConfiguration implements WebMvcConfigurer, ApplicationContextAware {
 
+    private final ThymeleafProperties thymeleafProperties;
     private final CustomViewConfiguration viewConfiguration;
     private final CustomResourceConfiguration resourceConfiguration;
     private final CustomUploadConfiguration uploadConfiguration;
 
+    private ApplicationContext applicationContext;
+
     @Autowired
-    public WebMvcConfiguration(CustomViewConfiguration viewConfiguration,
+    public WebMvcConfiguration(ThymeleafProperties thymeleafProperties,
+                               CustomViewConfiguration viewConfiguration,
                                CustomResourceConfiguration resourceConfiguration,
                                CustomUploadConfiguration uploadConfiguration) {
+        this.thymeleafProperties = thymeleafProperties;
         this.viewConfiguration = viewConfiguration;
         this.resourceConfiguration = resourceConfiguration;
         this.uploadConfiguration = uploadConfiguration;
     }
 
-    @Bean
-    public WebServerFactoryCustomizer<ConfigurableWebServerFactory> webServerFactoryCustomizer() {
-        return factory -> {
-            factory.addErrorPages(new ErrorPage(HttpStatus.BAD_REQUEST, "/error/400"));
-            factory.addErrorPages(new ErrorPage(HttpStatus.UNAUTHORIZED, "/error/401"));
-            factory.addErrorPages(new ErrorPage(HttpStatus.FORBIDDEN, "/error/403"));
-            factory.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/error/404"));
-            factory.addErrorPages(new ErrorPage(HttpStatus.METHOD_NOT_ALLOWED, "/error/405"));
-            factory.addErrorPages(new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error/500"));
-        };
-    }
-
-    @Resource
-    public void configureThymeleafStaticVars(ThymeleafViewResolver viewResolver) {
-        Map<String, Object> domain = Dict.create()
-                .set("IMAGE_DOMAIN", uploadConfiguration.getImage().getDomain());
-
-        viewResolver.setStaticVariables(domain);
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
     }
 
     @Override
@@ -91,5 +90,58 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
                 .allowedOrigins("*")
                 .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
                 .maxAge(3600);
+    }
+
+    @Bean
+    public WebServerFactoryCustomizer<ConfigurableWebServerFactory> webServerFactoryCustomizer() {
+        return factory -> {
+            factory.addErrorPages(new ErrorPage(HttpStatus.BAD_REQUEST, "/error/400"));
+            factory.addErrorPages(new ErrorPage(HttpStatus.UNAUTHORIZED, "/error/401"));
+            factory.addErrorPages(new ErrorPage(HttpStatus.FORBIDDEN, "/error/403"));
+            factory.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/error/404"));
+            factory.addErrorPages(new ErrorPage(HttpStatus.METHOD_NOT_ALLOWED, "/error/405"));
+            factory.addErrorPages(new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error/500"));
+        };
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver templateResolver() {
+        // SpringResourceTemplateResolver automatically integrates with Spring's own
+        // resource resolution infrastructure, which is highly recommended.
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setCharacterEncoding(thymeleafProperties.getEncoding().toString());
+        templateResolver.setPrefix(thymeleafProperties.getPrefix());
+        templateResolver.setSuffix(thymeleafProperties.getSuffix());
+        // HTML is the default value, added here for the sake of clarity.
+        templateResolver.setTemplateMode(thymeleafProperties.getMode());
+        // Template cache is true by default. Set to false if you want
+        // templates to be automatically updated when modified.
+        templateResolver.setCacheable(thymeleafProperties.isCache());
+        templateResolver.setApplicationContext(this.applicationContext);
+        return templateResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        // SpringTemplateEngine automatically applies SpringStandardDialect and
+        // enables Spring's own MessageSource message resolution mechanisms.
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        // Enabling the SpringEL compiler with Spring 4.2.4 or newer can
+        // speed up execution in most scenarios, but might be incompatible
+        // with specific cases when expressions in one template are reused
+        // across different data types, so this flag is "false" by default
+        // for safer backwards compatibility.
+        templateEngine.setEnableSpringELCompiler(true);
+        templateEngine.addDialect(new LayUITagDialect());
+        templateEngine.addDialect(new SpringSecurityDialect());
+        return templateEngine;
+    }
+
+    @Resource
+    public void configureThymeleafStaticVars(ThymeleafViewResolver viewResolver) {
+        Map<String, Object> domain = Dict.create()
+                .set("IMAGE_DOMAIN", uploadConfiguration.getImage().getDomain());
+        viewResolver.setStaticVariables(domain);
     }
 }
---
+++ springboot-template-admin/src/main/java/com/example/boot/thymeleaf/LayUITagDialect.java
@@ -0,0 +1,42 @@
+package com.example.boot.thymeleaf;
+
+import org.thymeleaf.dialect.AbstractProcessorDialect;
+import org.thymeleaf.processor.IProcessor;
+import org.thymeleaf.standard.StandardDialect;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 定义thymeleaf-layui的方言(th-lay:xxx)
+ *
+ * @author Chang
+ * @date 2019/10/21 22:19
+ */
+public class LayUITagDialect extends AbstractProcessorDialect {
+
+    private static final String DIALECT_NAME = "LAYUI_DIALECT";
+    private static final String DEFAULT_PREFIX = "layui";
+
+    public LayUITagDialect() {
+        // We will set this dialect the same "dialect processor" precedence as
+        // the Standard Dialect, so that processor executions can interleave.
+        super(DIALECT_NAME, DEFAULT_PREFIX, StandardDialect.PROCESSOR_PRECEDENCE);
+    }
+
+    /**
+     * 这里将定义好的标签处理器进行统一处理
+     * <p>
+     * Processor List
+     * 1.LayUIHrefTagProcessor：LayUI菜单组件的属性lay-href='xxx'
+     *
+     * @param dialectPrefix 方言前缀
+     * @return Set<IProcessor>
+     */
+    @Override
+    public Set<IProcessor> getProcessors(final String dialectPrefix) {
+        final Set<IProcessor> processors = new HashSet<>();
+        processors.add(new LayUIHrefTagProcessor(dialectPrefix));
+        return processors;
+    }
+}
