8059078bb728d42b47964d621f23351e4b55d222
Chang__ <chang_avalon@163.com>
2019-11-17 23:06:14 +0800

    这一周的改动成果;

--- springboot-template-cache/src/main/java/com/example/boot/springboottemplatecache/configuration/RedisConfiguration.java
+++
@@ -1,8 +0,0 @@
-package com.example.boot.springboottemplatecache.configuration;
-
-/**
- * springboot redis配置
- * Created by EDZ on 2019/9/25.
- */
-public class RedisConfiguration {
-}
--- springboot-template-cache/src/main/java/com/example/boot/springboottemplatecache/model/CacheRO.java
+++
@@ -1,7 +0,0 @@
-package com.example.boot.springboottemplatecache.model;
-
-/**
- * Created by EDZ on 2019/9/25.
- */
-public class CacheRO {
-}
