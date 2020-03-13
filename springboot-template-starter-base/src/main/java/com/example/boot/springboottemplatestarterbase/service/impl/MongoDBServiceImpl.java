2283b78fee8732852bf6fc8ed7e7e4c09a2c640b
chang_ <chang_avalon@163.com>
2020-03-13 16:50:09 +0800

    1.提交代码;

---
+++ springboot-template-starter-base/src/main/java/com/example/boot/springboottemplatestarterbase/service/impl/MongoDBServiceImpl.java
@@ -0,0 +1,12 @@
+package com.example.boot.springboottemplatestarterbase.service.impl;
+
+/**
+ * description:
+ * <p></p>
+ *
+ * @author chang_
+ * @date 27/2/2020
+ * @time 1:03 下午
+ **/
+public class MongoDBServiceImpl {
+}
